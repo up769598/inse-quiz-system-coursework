944fc5724900acdd510d552fc706d9a9ce41a2a8
UP776193 <33348872+UP776193@users.noreply.github.com>
2018-01-12 13:01:39 +0000

    New Netbeans Project

---
+++ Quiz System/QuizSystem/src/quizsystem/QuizSystem.java
@@ -0,0 +1,9 @@
+package quizsystem;
+
+public class QuizSystem {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ Quiz System/QuizSystem/src/quizsystem/User.java
@@ -0,0 +1,13 @@
+package quizsystem;
+
+public class User {
+    
+    private String email;
+    private String password;
+    
+    public User(String inEmail, String inPassword){
+        email = inEmail;
+        password = inPassword;
+    }
+    
+}
