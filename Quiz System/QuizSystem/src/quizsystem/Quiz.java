303d6394c2666c7bd25065eb871591020a4a8b26
up769598 <up769598@myport.ac.uk>
2018-02-16 12:18:44 +0000

    Added more methords to quiz and a test class

--- Quiz System/QuizSystem/src/quizsystem/Quiz.java
+++ Quiz System/QuizSystem/src/quizsystem/Quiz.java
@@ -18,22 +18,22 @@ public class Quiz {
     * first is for the question number second is the answer number
     */
    protected String[][] answers;
-   protected int correctAnswerIndex;
+   protected int[] correctAnswerIndex;
    protected int length;
+   protected int score;
     
    public Quiz(){
     
 }
  
    
-   public Quiz(int db_QuizID, String[] db_Questions, String[][] db_Answers, int db_CorrectAnswer){
+   public Quiz(int db_QuizID, String[] db_Questions, String[][] db_Answers, int[] db_CorrectAnswer){
        this.length = db_Questions.length;
        this.questions = db_Questions;
        this.correctAnswerIndex = db_CorrectAnswer;
-       this.quizID = db_QuizID;    
+       this.quizID = db_QuizID;
+       this.score = 0;
    }
-
-
 
 public String getQuestion (int questionNo){
        return this.questions[questionNo];
@@ -43,5 +43,15 @@ public String[] getAnswers (int questionNo){
     return this.answers[questionNo];
 }
 
+public boolean checkAnswer (int questionNo,int chosenAnswer ){
+    return correctAnswerIndex[questionNo] == chosenAnswer;
+}
+
+public void upScore(){
+    this.score++;
+}
+public int getScore(){
+    return this.score;
+}
 
 }
