3f45e853884d05d72ff24739c4f76e307ba57676
UP776193 <33348872+UP776193@users.noreply.github.com>
2018-02-18 17:48:24 +0000

    Fixed Prior Commit
    Mispell and new Constructer did not function with generated code. Both Fixed

--- Quiz System/QuizSystem/src/quizsystem/Quiz.java
+++ Quiz System/QuizSystem/src/quizsystem/Quiz.java
@@ -4,60 +4,97 @@ package quizsystem;
  *
  * @author 769598
  */
-
-
 public class Quiz {
-   protected int quizID;
-   protected String[] questions;
-   /**
-    * first is for the question number second is the answer number
-    */
-   protected String[][] answers;
-   protected int[] correctAnswerIndex;
-   protected int length;
-   protected int score;
-   protected int currentQuestion;
+
+    protected int quizID;
+    protected String[] questions;
+
+    /**
+     * first is for the question number second is the answer number
+     */
+    protected String[][] answers;
+    protected int[] correctAnswerIndex;
+    protected int length;
+    protected int score;
+    protected int currentQuestion;
+    protected String name;
+    protected String lecturerName;
+    protected float timelimit;
+
+    public Quiz() {
+
+    }
+
+    public Quiz(int db_QuizID, String[] db_Questions, String[][] db_Answers, int[] db_CorrectAnswer, String db_Name, String db_LecturerName, float db_Timelimit) {
+        this.length = db_Questions.length;
+        this.questions = db_Questions;
+        this.correctAnswerIndex = db_CorrectAnswer;
+        this.quizID = db_QuizID;
+        this.score = 0;
+        this.currentQuestion = 0;
+        this.name = db_Name;
+        this.lecturerName = db_LecturerName;
+        this.timelimit = db_Timelimit;
+    }
+
+    public float getTimelimit() {
+        return timelimit;
+    }
+
+    public void setTimelimit(float timelimit) {
+        this.timelimit = timelimit;
+    }
     
-   public Quiz(){
-    
-}
- 
-   
-   public Quiz(int db_QuizID, String[] db_Questions, String[][] db_Answers, int[] db_CorrectAnswer){
-       this.length = db_Questions.length;
-       this.questions = db_Questions;
-       this.correctAnswerIndex = db_CorrectAnswer;
-       this.quizID = db_QuizID;
-       this.score = 0;
-       this.currentQuestion = 0;
-   }
-  
+    public String getName() {
+        return name;
+    }
 
-public String getQuestion (int questionNo){
-       return this.questions[questionNo];
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public String[] getAnswers (int questionNo){
-    return this.answers[questionNo];
-}
+    public String getLecturerName() {
+        return lecturerName;
+    }
 
-public boolean checkAnswer (int questionNo,int chosenAnswer ){
-    return correctAnswerIndex[questionNo] == chosenAnswer;
-}
+    public void setLecturerName(String lecturerName) {
+        this.lecturerName = lecturerName;
+    }
 
-public void upScore(){
-    this.score++;
-}
-public int getScore(){
-    return this.score;
-}
+    public String[] getQuestions() {
+        return questions;
+    }
 
-public int getCurrentQuestion(){
-    return this.currentQuestion;
-}
+    public void setQuestions(String[] questions) {
+        this.questions = questions;
+    }
 
-public void setCurrentQuestion(int questionIndex){
-    this.currentQuestion = questionIndex;
-}
+    public String getQuestion(int questionNo) {
+        return this.questions[questionNo];
+    }
+
+    public String[] getAnswers(int questionNo) {
+        return this.answers[questionNo];
+    }
+
+    public boolean checkAnswer(int questionNo, int chosenAnswer) {
+        return correctAnswerIndex[questionNo] == chosenAnswer;
+    }
+
+    public void upScore() {
+        this.score++;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public int getCurrentQuestion() {
+        return this.currentQuestion;
+    }
+
+    public void setCurrentQuestion(int questionIndex) {
+        this.currentQuestion = questionIndex;
+    }
 
 }
